be1c28d968377ccd45c358642229bdb98201accc
Gabriel Prá <gabrielpra1@gmail.com>
2019-05-01 20:20:39 -0300

    Add project metadata

--- mix.exs
+++ mix.exs
@@ -7,7 +7,11 @@ defmodule Deribit.MixProject do
       version: "0.1.0",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      name: "Deribit",
+      source_url: "https://github.com/gabrielpra1/deribit-elixir",
+      description: " Deribit v2 API client for Elixir",
+      package: package()
     ]
   end
 
@@ -18,9 +22,21 @@ defmodule Deribit.MixProject do
     ]
   end
 
+  defp package do
+    [
+      files: ~w(lib mix.exs README*),
+      licenses: ["Apache 2.0"],
+      links: %{
+        "GitHub" => "https://github.com/gabrielpra1/deribit-elixir",
+        "Docs" => "https://hexdocs.pm/crudry/"
+      }
+    ]
+  end
+
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:ex_doc, "~> 0.19", only: :dev, runtime: false},
       {:httpoison, "~> 1.4"},
       {:jason, "~> 1.1"}
     ]
